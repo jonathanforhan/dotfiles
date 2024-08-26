82a10d50bced5b6bc34a702194ca4b1ca0f17b03
Jonathan Forhan <jon@pop-os.lan>
2024-08-26 00:41:07 -0400

    switch to stable 24.05

--- nix/flake.nix
+++ nix/flake.nix
@@ -2,9 +2,9 @@
   description = "Home Manager configuration of jon";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
