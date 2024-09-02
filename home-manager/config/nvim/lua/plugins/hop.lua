15728d744a2de613b5fb521300c35609e49f8113
jonathanforhan <99012095+jonathanforhan@users.noreply.github.com>
2024-09-02 16:42:23 -0400

    Delete home-manager directory

--- home-manager/config/nvim/lua/plugins/hop.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "smoka7/hop.nvim",
-  version = "*",
-  cmd = "HopWord",
-  init = function()
-    vim.keymap.set("n", "<LEADER>h", ":HopWord<CR>", { desc = "Hop Word" })
-  end,
-  config = true
-}
